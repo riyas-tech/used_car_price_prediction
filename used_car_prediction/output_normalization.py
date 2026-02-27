b3dc250298976274cff0aca2643de3eddcca5f3a
riyas-tech <mohamedr.alungal@gmail.com>
2026-02-26 22:05:41 -0500

    added normalization

--- used_car_prediction/output_normalization.py
+++ used_car_prediction/output_normalization.py
@@ -4,7 +4,6 @@ import torch
 import torch
 import pandas as pd
 from torch import nn
-import sys
 
 df = pd.read_csv("D:\\nlp_workrepo\\ml_learning\\used_car_prediction\\data\\used_cars.csv")
 
@@ -20,7 +19,7 @@ milage = df["milage"]
 milage = milage.str.replace("," , "")
 milage = milage.str.replace(" mi.", "")
 milage = milage.astype(int)
- 
+
 
 # Create Data as Tensors 
 X= torch.column_stack([
@@ -28,13 +27,11 @@ X= torch.column_stack([
     torch.tensor(milage, dtype=torch.float32)
 ]
 )
-
-X_mean = X.mean(axis=0)
-X_std = X.std(axis=0)
-X = (X - X_mean) / X_std
-
+print(X)
 
 Y = torch.tensor(price, dtype=torch.float32).reshape((-1, 1))
+print(Y)
+
 y_mean = Y.mean()
 y_std = Y.std()
 Y = (Y - y_mean) / y_std
@@ -42,9 +39,9 @@ Y = (Y - y_mean) / y_std
 
 model = nn.Linear(2, 1)
 loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.00000000001)
 
-for i in range(0, 10000):
+for i in range(0, 1000):
     optimizer.zero_grad()
     ouputs = model(X)
     loss = loss_fn(ouputs, Y)
@@ -52,10 +49,12 @@ for i in range(0, 10000):
     optimizer.step()
 
     # if i % 100 == 0:
-    #     print(loss)
+    #     print(model.bias)
+    #     print(model.weight)
+prediction = model(torch.tensor([
+    [5.0, 10000.0],
+    [5.0, 10000.0]
+], dtype=torch.float32))
 
-x_data = torch.tensor([[5,10000], [2, 10000], [5, 20000]], dtype=torch.float32)    
-
-prediction = model((x_data - X_mean) / X_std)
 
 print(prediction * y_std + y_mean)
--- used_car_prediction/training.py
+++ used_car_prediction/training.py
@@ -4,7 +4,6 @@ import torch
 import torch
 import pandas as pd
 from torch import nn
-import sys
 
 df = pd.read_csv("D:\\nlp_workrepo\\ml_learning\\used_car_prediction\\data\\used_cars.csv")
 
@@ -20,7 +19,7 @@ milage = df["milage"]
 milage = milage.str.replace("," , "")
 milage = milage.str.replace(" mi.", "")
 milage = milage.astype(int)
- 
+
 
 # Create Data as Tensors 
 X= torch.column_stack([
@@ -28,23 +27,16 @@ X= torch.column_stack([
     torch.tensor(milage, dtype=torch.float32)
 ]
 )
-
-X_mean = X.mean(axis=0)
-X_std = X.std(axis=0)
-X = (X - X_mean) / X_std
-
+print(X)
 
 Y = torch.tensor(price, dtype=torch.float32).reshape((-1, 1))
-y_mean = Y.mean()
-y_std = Y.std()
-Y = (Y - y_mean) / y_std
-
+print(Y)
 
 model = nn.Linear(2, 1)
 loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.0000000001)
 
-for i in range(0, 10000):
+for i in range(0, 1000):
     optimizer.zero_grad()
     ouputs = model(X)
     loss = loss_fn(ouputs, Y)
@@ -52,10 +44,11 @@ for i in range(0, 10000):
     optimizer.step()
 
     # if i % 100 == 0:
-    #     print(loss)
+    #     print(model.bias)
+    #     print(model.weight)
+prediction = model(torch.tensor([
+    [5.0, 10000.0],
+    [5.0, 10000.0]
+], dtype=torch.float32))
 
-x_data = torch.tensor([[5,10000], [2, 10000], [5, 20000]], dtype=torch.float32)    
-
-prediction = model((x_data - X_mean) / X_std)
-
-print(prediction * y_std + y_mean)
+print(prediction)
